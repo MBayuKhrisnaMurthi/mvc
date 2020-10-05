fd9e4e8b67fef46fb2daaef2ec08853b8ff050eb
MBayuKhrisnaMurthi <jagokandang1204@gmail.com>
2020-10-05 17:35:15 +0700

    fixing url.2

--- app/core/App.php
+++ app/core/App.php
@@ -12,17 +12,19 @@ class App{
         //cek apakah ada didalam contollers
         if (file_exists('../app/controllers/' . $url[0] . '.php')) {
             $this->controller = $url[0];
+            unset ($url[0]);
         }
 
         //call fuct
-        require_once '../app/controllers/' . $url[0] . '.php';
+        require_once '../app/controllers/' . $this->controller . '.php';
         $this->controller = new $this->controller;
-        unset ($url[0]);
-
+        
         //cek method
-        if (method_exists($this->controller, $url[1])) {
-            $this->method = $url[1];
-            unset ($url[1]);
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset ($url[1]);
+            }
         }
 
         //cek params
