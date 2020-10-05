154598f5391a695e5a2cbc3026ec1817c69e1b17
MBayuKhrisnaMurthi <jagokandang1204@gmail.com>
2020-10-05 15:11:00 +0700

    default controller method and bring params if he has

---
+++ app/controllers/Home.php
@@ -0,0 +1,9 @@
+<?php
+
+class Home {
+    public function Index($name = 'bayu', $age = 23, $hobby = 'trader') {
+        echo "Home/Index <br>";
+        echo "Yo guys its me $name, im $age years old and my hobby is $hobby <br>";
+        echo "sorry for bad grammar";
+    }
+}
--- app/core/App.php
+++ app/core/App.php
@@ -1,10 +1,37 @@
 <?php
 
 class App{
-    public function __construct()
-    {
+    //default
+    protected $controller = 'Home';
+    protected $method = 'Index';
+    protected $params = [];
+
+    public function __construct(){
         $url = $this->parse_url();
-        var_dump($url);   
+        
+        //cek apakah ada didalam contollers
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+        }
+
+        //call fuct
+        require_once '../app/controllers/' . $url[0] . '.php';
+        $this->controller = new $this->controller;
+        unset ($url[0]);
+
+        //cek method
+        if (method_exists($this->controller, $url[1])) {
+            $this->method = $url[1];
+            unset ($url[1]);
+        }
+
+        //cek params
+        if (!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
     }
     public function parse_url(){
         if (isset($_GET['url'])) {
